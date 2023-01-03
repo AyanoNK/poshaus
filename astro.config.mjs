b10a66fd6eb22ccdf51321235752199bfc2b5685
Ayano <s.correa.wilches@gmail.com>
2023-01-02 22:53:18 -0500

    Add sharp entrypoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import react from "@astrojs/react";
@@ -8,5 +8,10 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), image()]
+  integrations: [
+    react(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
