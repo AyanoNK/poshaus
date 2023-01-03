4ef6767b79794cc89a43f521d0aa2a1bba8fc733
Ayano <s.correa.wilches@gmail.com>
2023-01-02 23:11:43 -0500

    Add base image optimization

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,9 @@
-/// <reference types="astro/client" />
+/// <reference types="@astrojs/image/client" />
 
 interface ImportMetaEnv {
-	readonly PUBLIC_VERCEL_ANALYTICS_ID: string;
+  readonly PUBLIC_VERCEL_ANALYTICS_ID: string;
 }
 
 interface ImportMeta {
-	readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv;
 }
