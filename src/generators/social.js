acda3255fa3bb8d6ccce5d144ebc849139615e5c
Ayano <s.correa.wilches@gmail.com>
2023-01-07 20:16:19 -0500

    feat: add footer & header on galleries (#2)
    * Remove extra on topper
    
    * Update images with optimized imgs
    
    * Remove useless library
    
    * Add source set for mutiple images
    
    * Add robots.txt
    
    * Add header & footer
    
    * Remove unused libs

---
+++ src/generators/social.js
@@ -0,0 +1,20 @@
+const social = [
+  {
+    name: "instagram",
+    url: "https://www.instagram.com/poshaus/",
+  },
+  {
+    name: "behance",
+    url: "https://www.behance.net/poshaus",
+  },
+  {
+    name: "twitter",
+    url: "https://twitter.com/DrColdy",
+  },
+  {
+    name: "email",
+    url: "mailto:jr.chand204@gmail.com",
+  },
+];
+
+export default social;
