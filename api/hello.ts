49f466b0ee24e9a4224aa7c8f19d484bf9124b77
Ayano <s.correa.wilches@gmail.com>
2023-04-07 00:16:35 -0500

    add async to func

--- api/hello.ts
+++ api/hello.ts
@@ -2,15 +2,14 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import ytdl from "ytdl-core";
 const fs = require("fs");
 
-export default function handler(
+export default async function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
-  const info = ytdl.getInfo("dQw4w9WgXcQ");
+  const info = await ytdl.getInfo("dQw4w9WgXcQ");
 
   response.status(200).json({
-    response: info,
-    body: request.body,
+    body: info,
     query: request.query,
     cookies: request.cookies,
   });
