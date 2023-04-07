c936eab1a1adefa6baeb98501c5bff896b3c9827
Ayano <s.correa.wilches@gmail.com>
2023-04-07 01:45:49 -0500

    fix ytdl core version

--- api/hello.ts
+++ api/hello.ts
@@ -1,15 +1,29 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import ytdl from "ytdl-core";
-const fs = require("fs");
 
 export default async function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
-  const info = await ytdl.getInfo("dQw4w9WgXcQ");
+  const maxBitrateAudio = await ytdl
+    .getInfo("7wNb0pHyGuI1491621961865")
+    .then((info) => {
+      return info.formats
+        .filter((f) => f.mimeType?.includes("audio"))
+        .reduce((accumulator, current) => {
+          if (
+            accumulator.bitrate &&
+            current.bitrate &&
+            accumulator.bitrate > current.bitrate
+          )
+            return accumulator;
+          else return current;
+        });
+    })
+    .catch(() => null);
 
   response.status(200).json({
-    body: info,
+    body: { url: maxBitrateAudio ? maxBitrateAudio.url : null },
     query: request.query,
     cookies: request.cookies,
   });
