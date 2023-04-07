ba857890b3232dc0f36a644eb6e862b55a2dbb0b
Ayano <s.correa.wilches@gmail.com>
2023-04-07 00:08:41 -0500

    add vercel node

--- api/hello.ts
+++ api/hello.ts
@@ -1,7 +1,17 @@
-export const config = {
-  runtime: "edge",
-};
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import ytdl from "ytdl-core";
+const fs = require("fs");
 
-export default (request: Request) => {
-  return new Response(`Hello, from ${request.url} I'm now an Edge Function!`);
-};
+export default function handler(
+  request: VercelRequest,
+  response: VercelResponse
+) {
+  const info = ytdl.getInfo("dQw4w9WgXcQ");
+
+  response.status(200).json({
+    response: info,
+    body: request.body,
+    query: request.query,
+    cookies: request.cookies,
+  });
+}
