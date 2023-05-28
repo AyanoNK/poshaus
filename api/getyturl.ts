487efa4e20862f97db0fb208a1482a47a411b6e6
Ayano <s.correa.wilches@gmail.com>
2023-05-27 21:22:45 -0500

    add cors to request

--- api/getyturl.ts
+++ api/getyturl.ts
@@ -1,10 +1,27 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import ytdl from "ytdl-core";
 
-export default async function handler(
-  request: VercelRequest,
-  response: VercelResponse
-) {
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+async function handler(request: VercelRequest, response: VercelResponse) {
   const id = request.query.id as string;
 
   if (!id)
@@ -32,3 +49,5 @@ export default async function handler(
     url: maxBitrateAudio ? maxBitrateAudio.url : null,
   });
 }
+
+module.exports = allowCors(handler);
