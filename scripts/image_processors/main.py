df5d78624a981e24dc4450f240bf2fb1b76e5dfc
Ayano <s.correa.wilches@gmail.com>
2025-04-17 15:59:31 -0500

    edit base script to take on dynamic resolutions

--- scripts/image_processors/main.py
+++ scripts/image_processors/main.py
@@ -2,20 +2,12 @@ import os
 from PIL import Image
 
 
-# take all files and resize them to have a max width or height of 300px
 # maintain aspect ratio
 # suppose all images are webp
 illustrations_directory = "../../public/img/illustrations"
 
 
-def rename_original_image(image_path):
-    # take the original image and rename it to have a _original suffix
-    # check if the image is already renamed
-    original_image_filename = os.path.splitext(image_path)[0] + "_original.webp"
-    print(f"Renaming image {image_path} to {original_image_filename}")
-
-    os.rename(image_path, original_image_filename)
-    print(f"Renamed image {image_path} to {original_image_filename}")
+accepted_sizes = [640, 768, 1024, 1280, 1536]
 
 
 def resize_image(image_path, max_size):
@@ -24,20 +16,22 @@ def resize_image(image_path, max_size):
 
         new_image_filename = os.path.splitext(image_path)[0] + f"_{max_size}.webp"
         if os.path.exists(new_image_filename):
-            print(f"Image {new_image_filename} already exists. Skipping.")
+            print(
+                f"Original image {image_path} already resized to {max_size}. Skipping."
+            )
             return
 
         # check if the image is already the right size
-        if img.size[0] <= max_size and img.size[1] <= max_size:
-            print(f"Image {image_path} is already the right size. Skipping.")
-            return
+        # if img.size[0] <= max_size:
+        #     print(f"Image {image_path} is already the right size. Skipping.")
+        #     return
 
         # resize image
         print(f"Resizing image {image_path} to {max_size}px")
         sizes = (max_size, max_size)
         new_image = img.copy()
         new_image.thumbnail(sizes, Image.Resampling.LANCZOS)
-        new_image.save(new_image_filename, format="webp", quality=100)
+        new_image.save(new_image_filename, format="webp")
 
 
 def resize_images_in_directory(directory, max_size):
@@ -46,21 +40,16 @@ def resize_images_in_directory(directory, max_size):
             print("Skipping non-webp file:", filename)
             continue
 
-        if str(max_size) in filename:
-            print(f"Image {filename} already resized. Skipping.")
-            continue
-
-        if "_original" in filename:
-            print(f"Image {filename} already has _original suffix. Skipping.")
+        if any(str(size) in filename for size in accepted_sizes):
+            print(f"Image {filename} already resized previously. Skipping.")
             continue
 
         # get absolute path of the image
         image_path = os.path.join(directory, filename)
 
         resize_image(image_path, max_size)
-        rename_original_image(image_path)
 
 
 if __name__ == "__main__":
-    resize_images_in_directory(illustrations_directory, max_size=300)
+    resize_images_in_directory(illustrations_directory, max_size=1536)
     print("All images resized.")
